0cbb25b9ac9cfdfd27902a214f5cf69f9e59f4fc
hugosenari <hugosenari@gmail.com>
2022-03-12 03:32:53 -0300

    chore: re index mofuw

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''mofuw is *MO*re *F*aster, *U*ltra *W*ebserver'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."mofuw-master".url = "path:./master";
+  inputs."mofuw-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mofuw-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
